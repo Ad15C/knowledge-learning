9d4ee573c5adb7469cb20cd6ba1349491c9a1c83
Ad15C <ad15canon@hotmail.com>
2026-02-19 15:26:01 +0100

    cursus-controller

--- src/Entity/PurchaseItem.php
+++ src/Entity/PurchaseItem.php
@@ -18,61 +18,31 @@ class PurchaseItem
     private ?Purchase $purchase = null;
 
     #[ORM\ManyToOne(targetEntity: Cursus::class)]
-    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\JoinColumn(nullable: true)] 
     private ?Cursus $cursus = null;
+
+    #[ORM\ManyToOne(targetEntity: Lesson::class)]
+    #[ORM\JoinColumn(nullable: true)]
+    private ?Lesson $lesson = null;
 
     #[ORM\Column(type: 'float')]
     private ?float $price = null;
 
     #[ORM\Column]
-    private ?int $quantity = 1;
+    public function getId(): ?int { return $this->id; }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+    public function getPurchase(): ?Purchase { return $this->purchase; }
+    public function setPurchase(?Purchase $purchase): static { $this->purchase = $purchase; return $this; }
 
-    public function getPurchase(): ?Purchase
-    {
-        return $this->purchase;
-    }
+    public function getCursus(): ?Cursus { return $this->cursus; }
+    public function setCursus(?Cursus $cursus): static { $this->cursus = $cursus; return $this; }
 
-    public function setPurchase(?Purchase $purchase): static
-    {
-        $this->purchase = $purchase;
-        return $this;
-    }
+    public function getLesson(): ?Lesson { return $this->lesson; }
+    public function setLesson(?Lesson $lesson): static { $this->lesson = $lesson; return $this; }
 
-    public function getCursus(): ?Cursus
-    {
-        return $this->cursus;
-    }
+    public function getPrice(): ?float { return $this->price; }
+    public function setPrice(float $price): static { $this->price = $price; return $this; }
 
-    public function setCursus(?Cursus $cursus): static
-    {
-        $this->cursus = $cursus;
-        return $this;
-    }
-
-    public function getPrice(): ?float
-    {
-        return $this->price;
-    }
-
-    public function setPrice(float $price): static
-    {
-        $this->price = $price;
-        return $this;
-    }
-
-    public function getQuantity(): ?int
-    {
-        return $this->quantity;
-    }
-
-    public function setQuantity(int $quantity): static
-    {
-        $this->quantity = $quantity;
-        return $this;
-    }
+    public function getQuantity(): ?int { return $this->quantity; }
+    public function setQuantity(int $quantity): static { $this->quantity = $quantity; return $this; }
 }
