778bb7c3577b5fb176cfe3d14e359563c89d9fbd
Ad15C <ad15canon@hotmail.com>
2026-03-03 17:39:00 +0100

    admin_cursus_edit

--- src/Entity/Cursus.php
+++ src/Entity/Cursus.php
@@ -6,6 +6,7 @@ use App\Repository\CursusRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CursusRepository::class)]
 class Cursus
@@ -16,6 +17,7 @@ class Cursus
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Le nom est obligatoire.')]
     private ?string $name = null;
 
     #[ORM\Column(type: 'decimal', precision: 10, scale: 2)]
@@ -50,7 +52,11 @@ class Cursus
     public function getId(): ?int { return $this->id; }
 
     public function getName(): ?string { return $this->name; }
-    public function setName(string $name): static { $this->name = $name; return $this; }
+    public function setName(string $name): static
+{
+    $this->name = $name;
+    return $this;
+}
 
     public function getPrice(): ?float
     {
